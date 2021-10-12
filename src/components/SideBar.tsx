9f52e7877a76de8d6e106496200d6aeaefab3a02
Rubens Gouveia <rubensgui.gouveia@gmail.com>
2021-10-12 11:19:50 -0300

    creating components

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,28 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from '../components/Button';
+import {GenreResponseProps , MovieProps} from '../App'
+
+interface SideBarProps{
+  genres: GenreResponseProps[]
+  selectedGenreId: number
+  onClick:(id:number)=> void
+}
+export function SideBar({genres, selectedGenreId, onClick}: SideBarProps) {
+  return(
+    <nav className="sidebar">
+    <span>Watch<p>Me</p></span>
+
+    <div className="buttons-container">
+      {genres.map(genre => (
+        <Button
+          key={String(genre.id)}
+          title={genre.title}
+          iconName={genre.name}
+          onClick={() => onClick(genre.id)}
+          selected={selectedGenreId === genre.id}
+        />
+      ))}
+    </div>
+
+  </nav>
+  )
 }
